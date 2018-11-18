369aecde7e59679e3f65b932dcc34ac38d60ce94
Rigved Patki <rigved.patki@gmail.com>
2018-11-18 17:23:41 +0100

    modified triple Confidence

--- src/get-triple-confidence.ts
+++ src/get-triple-confidence.ts
@@ -11,76 +11,43 @@ const getTripleConfidence = (
   for (let tripleItem in tripleItemCount) {
     let [itemA, itemB, itemC] = tripleItem.split(',');
     let confABToC =
-      tripleItemCount[`${itemA},${itemB},${itemC}`] /
-      singleItemCount[`${itemC}`];
+      tripleItemCount[tripleItem] / doubleItemCount[`${itemA},${itemB}`];
     if (confABToC > config.CONFIDENCE) {
       confidenceTripleString =
         confidenceTripleString +
         `\nConfidence (${itemA},${itemB} -> ${itemC}) = ${confABToC}`;
     }
-    let confCToAB =
-      tripleItemCount[`${itemA},${itemB},${itemC}`] /
-      doubleItemCount[`${itemA},${itemB}`];
+    let confCToAB = tripleItemCount[tripleItem] / singleItemCount[`${itemC}`];
     if (confCToAB > config.CONFIDENCE) {
       confidenceTripleString =
         confidenceTripleString +
         `\nConfidence (${itemC} -> ${itemA},${itemB}) = ${confCToAB}`;
     }
-    let confCToBA =
-      tripleItemCount[`${itemA},${itemB},${itemC}`] /
-      doubleItemCount[`${itemB},${itemA}`];
-    if (confCToBA > config.CONFIDENCE) {
-      confidenceTripleString =
-        confidenceTripleString +
-        `\nConfidence (${itemC} -> ${itemB},${itemA}) = ${confCToBA}`;
-    }
     let confBCToA =
-      tripleItemCount[`$${itemA},${itemB},${itemC}`] /
-      singleItemCount[`${itemA}`];
+      tripleItemCount[tripleItem] / doubleItemCount[`${itemB},${itemC}`];
     if (confBCToA > config.CONFIDENCE) {
       confidenceTripleString =
         confidenceTripleString +
         ` \nConfidence (${itemB},${itemC} -> ${itemA}) = ${confBCToA}`;
     }
-    let confAToBC =
-      tripleItemCount[`${itemA},${itemB},${itemC}`] /
-      doubleItemCount[`${itemB},${itemC}`];
+    let confAToBC = tripleItemCount[tripleItem] / singleItemCount[`${itemA}`];
     if (confAToBC > config.CONFIDENCE) {
       confidenceTripleString =
         confidenceTripleString +
         `\nConfidence (${itemA} -> ${itemB},${itemC}) = ${confAToBC}`;
     }
-    let confAToCB =
-      tripleItemCount[`${itemA},${itemB},${itemC}`] /
-      doubleItemCount[`${itemC},${itemB}`];
-    if (confAToCB > config.CONFIDENCE) {
-      confidenceTripleString =
-        confidenceTripleString +
-        `\nConfidence (${itemA} -> ${itemC},${itemB}) = ${confAToCB}`;
-    }
     let confCAToB =
-      tripleItemCount[`${itemA},${itemB},${itemC}`] /
-      singleItemCount[`${itemB}`];
+      tripleItemCount[tripleItem] / doubleItemCount[`${itemC},${itemA}`];
     if (confCAToB > config.CONFIDENCE) {
       confidenceTripleString =
         confidenceTripleString +
         ` \nConfidence (${itemC},${itemA} -> ${itemB}) = ${confCAToB}`;
     }
-    let confBToCA =
-      tripleItemCount[`${itemA},${itemB},${itemC}`] /
-      doubleItemCount[`${itemC},${itemA}`];
+    let confBToCA = tripleItemCount[tripleItem] / singleItemCount[`${itemB}`];
     if (confBToCA > config.CONFIDENCE) {
       confidenceTripleString =
         confidenceTripleString +
         `\nConfidence (${itemB} -> ${itemC},${itemA}) = ${confBToCA}`;
-    }
-    let confBToAC =
-      tripleItemCount[`${itemA},${itemB},${itemC}`] /
-      doubleItemCount[`${itemA},${itemC}`];
-    if (confBToAC > config.CONFIDENCE) {
-      confidenceTripleString =
-        confidenceTripleString +
-        `\nConfidence (${itemB} -> ${itemA},${itemC}) = ${confBToAC}`;
     }
   }
   console.timeEnd('getTripleConfidence');
